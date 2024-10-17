5d0d0793900f8fc274d157b55661de9346c7914a
reyhanehgr <reyhaneh.gerami@gmail.com>
2024-10-17 08:39:53 -0700

    Upload Finding_Min_Max project on Github

---
+++ src/main/java/com/mycompany/finding_min_max/Finding_Min_Max.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.finding_min_max;
+
+import java.util.Scanner;
+
+//برنامه ای به زبان برنامه نویسی جاوا بنویسید که مینیمم و ماکزیمم عناصر موجود در یک آرایه را پیدا کند
+///Maximum and minimum of an array
+public class Finding_Min_Max {
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter size of Array:");
+        int size=scanner.nextInt();
+        int[] arr=new int[size];
+        for(int i=0;i<size;i++)
+        {
+            System.out.print("Enter a number:");
+            arr[i]=scanner.nextInt();
+        }
+        int max=arr[0];
+        int min=arr[0];
+        for(int i=0;i<size;i++){
+            if(arr[i]>max)
+                max=arr[i];
+            else if(arr[i]<min)
+                min=arr[i];
+            
+        }
+        System.out.println("min:"+min+" ,"+"max:"+max);
+    }
+}
